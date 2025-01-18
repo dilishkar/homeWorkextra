c03f53920a7fe0825eae4678e7457d3d48b42d76
dilmurat <dilmurat239nk@gmail.com>
2025-01-18 15:35:56 +0300

    create hw17

---
+++ main.js
@@ -0,0 +1,36 @@
+// task1------------------------------------------------------------------
+// let i = 0;
+// while (i <= 99) {
+//   i++;
+//   console.log(i);
+// }
+// task2------------------------------------------------------------------
+// let num = 1;
+// while (num < 100) {
+//   if (num % 2 == 0) console.log(num);
+//   num++;
+// }
+// task3--------------------------------------------------------------------
+// let summa = 0;
+// for (let i = 1; i <= 100; i++) {
+//   summa += i;
+// }
+// console.log(summa);
+// task4--------------------------------------------------------------------
+// let qwe = "Duolingo Green";
+// let char = "e";
+// let wert = qwe.length;
+// for (let count = 0; count <= wert; wert--) {
+//   const Char1 = qwe.charAt(wert);
+//   if (Char1 === char) {
+//     console.log(" да, там есть char-е ");
+//   }
+// }
+// task5--------------------------------------------------------------------
+// let student = "salam, how are you bro?";
+// let counter = student.length - 1;
+// while (counter >= 0) {
+//   console.log(student.charAt(counter));
+//   counter--;
+// }
+// task6--------------------------------------------------------------------
